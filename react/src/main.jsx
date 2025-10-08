b63751ee0d10c9fe356d6b28ad738a8c2308121c
mahajanP <prakashmah123@gmail.com>
2025-10-08 21:20:20 +0530

    [java project]Docker-Engine-for-Java-Project

---
+++ react/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://172.16.22.60:8080';
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
